0594ad629a689acd13d585417ef62be5af083e02
Daniel Huang <gn03249822@DanieltekiMacBook-Pro-2.local>
2018-01-11 11:21:42 -0800

    postgresql setup

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template, request
-
+from flask.ext.sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 @app.route("/")
@@ -10,7 +10,8 @@ def index():
 def success():
     if  request.method == "POST": #to make sure the user access this URL through a POST request
         email = request.form["email_name"] #grab the input value from email entry
-        print(email)
+        height = request.form["height_name"]
+        print(height)
         return render_template("success.html")
 
 if __name__ == "__main__": #meaning if the script is being executed, rather than being imported
